465cd8be46d4b4516db7a6eb94664a2d732446ae
spheroidon <spheroidon@gmail.com>
2024-01-01 20:02:26 +0200

    second

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from wallstreet import Stock
+from flask import Flask, render_template, request, jsonify
+import yfinance as yf
 
 app = Flask(__name__)
 
@@ -7,17 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/stock', methods=['POST'])
-def stock():
+@app.route('/get_stock_data', methods=['POST'])
+def get_stock_data():
     if request.method == 'POST':
-        symbol = request.form['symbol']
-        try:
-            stock = Stock(symbol)
-            price = stock.price
-            return render_template('stock.html', symbol=symbol, price=price, change=stock.change)
-        except:
-            error = "Invalid symbol or something went wrong."
-            return render_template('index.html', error=error)
+        ticker = request.form['ticker']
+        stock = yf.Ticker(ticker)
+        data = stock.history(period='3mo')  # You can adjust the period as needed
+        labels = data.index.strftime('%Y-%m-%d').tolist()
+        values = data['Close'].tolist()
+        return {'labels': labels, 'values': values}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=30025)
